ef557859bb981bd0fa395edbca62155e1fc2ee15
David Boddie <david@boddie.org.uk>
2013-06-27 22:49:45 +0200

    Moved the tools into a new directory and fixed a copyright line.

--- Tools/read_sprites.py
+++ Tools/read_sprites.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 """
-readmaps.py - Read Firetrack maps from a UEF file and write them to a text file.
+read_sprites.py - Extract Firetrack sprites from a UEF file to a directory.
 
-Copyright (C) 2010 David Boddie <david@boddie.org.uk>
+Copyright (C) 2013 David Boddie <david@boddie.org.uk>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
